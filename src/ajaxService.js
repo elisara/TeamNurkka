30c9fae73ad1ec9af23bd00df78b1e75b5121442
Keni <keni.kastinen@metropolia.fi>
2016-02-01 17:19:20 +0200

    https://github.com/elisara/TeamNurkka/tree/testBranch

--- src/ajaxService.js
+++ src/ajaxService.js
@@ -1,6 +1,6 @@
 angular.module('myApp')
     .factory('ajaxService', function ($http) {
-        return $http.get('http://util.mw.metropolia.fi/ImageRekt/api/v2/files')
+        return $http.get('http://util.mw.metropolia.fi/ImageRekt/api/v2/files/type/image')
             .success(function (data) {
                 return data;
             })

a6aa8b0a4165083c50b53f80332a56f26aa2ef8c
Elisa Rajaniemi <elisa.rajaniemi@gmail.com>
2016-03-06 15:45:03 +0200

    imageja 30/page

--- src/scripts/controllers/fileTypeController.js
+++ src/scripts/controllers/fileTypeController.js
@@ -6,7 +6,29 @@ angular.module('myApp')
 
         ajaxFactory.getImageFiles().success(function (data) {
             $scope.imageFiles = data;
+            $scope.itemsPerPage = 30;
+            $scope.currentPage = 0;
+            $scope.total = $scope.imageFiles.length;
+            $scope.imagepagedFiles = $scope.imageFiles.slice($scope.currentPage * $scope.itemsPerPage,
+                $scope.currentPage * $scope.itemsPerPage + $scope.itemsPerPage);
+            
+            $scope.loadMore = function () {
+                $scope.currentPage++;
+                var newItems = $scope.imageFiles.slice($scope.currentPage * $scope.itemsPerPage,
+                    $scope.currentPage * $scope.itemsPerPage + $scope.itemsPerPage);
+                $scope.imagepagedFiles = $scope.imagepagedFiles.concat(newItems);
+            };
+
+            $scope.nextPageDisabledClass = function () {
+                return $scope.currentPage === $scope.pageCount() - 1 ? "disabled" : "";
+            };
+
+            $scope.pageCount = function () {
+                return Math.ceil($scope.total / $scope.itemsPerPage);
+            };
         });
+        
+    
 
         ajaxFactory.getVideoFiles().success(function (data) {
             $scope.videoFiles = data;
